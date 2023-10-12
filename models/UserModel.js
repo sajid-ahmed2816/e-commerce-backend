b8785a0b0e273ca90e79386a55f83b6a1af2c96c
sajid-ahmed2816 <sajid.ahmed2816@gmail.com>
2023-10-13 02:19:04 +0500

    login and signin route

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: Number,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -3,11 +3,12 @@ const route = express.Router();
 const { SendResponse } = require("../helper/SendResponse");
 const UserModel = require("../models/UserModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 route.post("/signup", async (req, res) => {
-  const { email, password } = req.body;
-  const obj = { email, password };
-  let reqArr = ["email", "password"];
+  const { firstName, lastName, email, password, contact } = req.body;
+  const obj = { firstName, lastName, email, password, contact };
+  let reqArr = ["firstName", "lastName", "email", "password", "contact"];
   let errArr = [];
 
   reqArr.forEach((item) => {
@@ -34,6 +35,30 @@ route.post("/signup", async (req, res) => {
     }
   } catch (error) {
     res.send(error).status(404);
+  }
+});
+
+route.get("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const obj = { email, password };
+  let result = await UserModel.findOne({ email });
+  console.log(result);
+  if (result) {
+    let confirm = await bcrypt.compare(obj.password, result.password);
+    if (confirm) {
+      let token = jwt.sign({ email }, process.env.SECURE_KEY, {
+        expiresIn: "24h",
+      });
+      res
+        .send(SendResponse(true, { user: result, token }, "Login Successfully"))
+        .status(200);
+    } else {
+      res
+        .send(SendResponse(true, null, "Invalid email or password"))
+        .status(400);
+    }
+  } else {
+    res.send(SendResponse(false, null, "User doesn't exist"));
   }
 });
 
